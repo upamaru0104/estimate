fe05497e95187e89e3f337eb1a91f94d1e2a245a
upamaru0104 <upakame0104@gmail.com>
2023-09-06 23:38:11 +0900

    マイグレーションファイル修正

--- db/migrate/20230906034426_create_estimate_products.rb
+++ db/migrate/20230906034426_create_estimate_products.rb
@@ -2,12 +2,12 @@ class CreateEstimateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :estimate_products do |t|
       t.string :estimate_number,              null: false, default: "", unique: true
-      t.date   :estimate_date,              null: false, default: ""
-      t.string :estimate_number,              null: false, default: ""
+      t.date   :estimate_date,              null: false
+      t.string :company_name,              null: false, default: ""
       t.integer :total_price,              null: false
       t.string :ancestry,              null: false, default: ""
       t.integer :quantity,              null: false
-      t.references :user,              null: false, default: "", foreign_key: true
+      t.references :user,              null: false, foreign_key: true
 
       t.timestamps
     end
