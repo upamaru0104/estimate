4db3f3d90a0d73fdd1bc7cb5a64a7dd5264d0d62
upamaru0104 <upakame0104@gmail.com>
2023-09-06 22:59:08 +0900

    バリデーションの記述

--- app/models/estimate_product.rb
+++ app/models/estimate_product.rb
@@ -1,2 +1,13 @@
 class EstimateProduct < ApplicationRecord
+  with_options presence: true do
+    validates :estimate_number
+    validates :estimate_date
+    validates :estimate_number
+    validates :total_price
+    validates :ancestry
+    validates :quantity
+  end
+
+  belongs_to :user
+
 end
