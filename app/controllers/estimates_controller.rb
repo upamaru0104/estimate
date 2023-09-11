9101c4073d00ae0099727b244b6f8d1b149d592f
upamaru0104 <upakame0104@gmail.com>
2023-09-11 20:21:12 +0900

    コメントアウトの削除

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -9,18 +9,18 @@ class EstimatesController < ApplicationController
   end
 
   def create
-    # binding.pry
   @estimate_product = EstimateProduct.new(estimate_product_params)
   date_change
-    # if @estimate_product.save
+  binding.pry
+    if @estimate_product.save
       post_pdf = EstimatePdf::PostPdf.new(@estimate_product, current_user)
       send_data post_pdf.render,
       filename: "#{@estimate_product.estimate_number}.pdf", # 作成されるファイル名を見積書番号に変更する
       type: 'application/pdf',
       disposition: 'inline' # 外すとダウンロード
-    # else
-    #   render :new
-    # end
+    else
+      render :new
+    end
   end
 
   private
