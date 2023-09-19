7a5d4308c6be1bf4a269f6c0ac47dc7d268bce4d
upamaru0104 <upakame0104@gmail.com>
2023-09-19 22:45:47 +0900

    修正
    保存するパラムスの修正
    階層型データベースをビューファイルに表示するための記述
    jsファイルで使用するsearchメソッドの定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'estimates/index'
   root to: "estimates#index"
   resources :estimates
+  get '/estimate/:id', to: 'estimates#search'
   resources :users, only: :show
 end
